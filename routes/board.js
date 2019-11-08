ed55c3d46116d96d46bbbc10ecae61d6f09ae80c
John Smith <jsmith@avansoft.co.kr>
2019-11-08 15:51:00 +0900

    2019-11-08

--- routes/board.js
+++ routes/board.js
@@ -12,6 +12,14 @@ let connection = mariadb.createConnection({
     multipleStatements: true
 });
 
+const del = connection._protocol._delegateError;
+connection._protocol._delegateError = function(err, sequence){
+    if (err.fatal) {
+        console.trace('fatal error: ' + err.message);
+    }
+    return del.call(this, err, sequence);
+};
+
 
 /* GET List Page */
 router.get('/', function (req, res) {
